f8d5a310c5121a59f649e88cbcc8fdbc71d19878
Alex Hunt <hello@alexhunt.io>
2020-05-23 15:37:59 +0100

    macos: Disable UI sound effects

--- macos/.macos-prefs/sound.sh
+++ macos/.macos-prefs/sound.sh
@@ -1,5 +1,8 @@
 # Disable sound effects on boot
 sudo nvram SystemAudioV
 
-# Stop iTunes from responding to the keyboard media keys
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/nullolume=" "
+# Disable user interface sound effects
+defaults write com.apple.systemsound com.apple.sound.uiaudio.enabled -bool false
+
+# Stop Apple Music from responding to the keyboard media keys
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
