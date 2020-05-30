21c36050bf8a8e08eea9523088fcaddfd02513d1
Alex Hunt <hello@alexhunt.io>
2020-05-30 11:26:58 +0100

    macos: Remove keyboard media keys override
    This seemed to prevent all media key handling on a Touch Bar MacBook. Undoable with `launchctl load -w /System/Library/LaunchAgents/com.apple.rcd.plist`.

--- macos/.macos-prefs/sound.sh
+++ macos/.macos-prefs/sound.sh
@@ -3,6 +3,3 @@ sudo nvram SystemAudioV
 
 # Disable user interface sound effects
 defaults write com.apple.systemsound com.apple.sound.uiaudio.enabled -bool false
-
-# Stop Apple Music from responding to the keyboard media keys
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
