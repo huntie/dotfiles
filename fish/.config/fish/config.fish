3cf6d6b1f3e8c9e04ba73c71bb7f776272182eac
Alex Hunt <hello@alexhunt.io>
2021-02-12 14:58:19 +0000

    fish: Fix Fisher bootstrap logic
    See jorgebucaran/fisher#644

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -59,8 +59,6 @@ end
 # - Packages -
 
 # Install and run Fisher package manager on first run
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+if status is-interactive && not functions -q fisher
+    curl -sL https://git.io/fisher | source; and fisher install jorgebucaran/fisher
 end
