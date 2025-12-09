a3f2adb725a467e5e0150611cb266702a1a0d072
Alex Hunt <hello@alexhunt.dev>
2025-12-09 13:58:01 +0000

    fish: Set colors explicitly

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -70,6 +70,32 @@ else
     alias open xdg-open
 end
 
+# - Colors -
+
+set -U fish_color_autosuggestion 555 brblack
+set -U fish_color_cancel -r
+set -U fish_color_command --bold
+set -U fish_color_comment red
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end brmagenta
+set -U fish_color_error brred
+set -U fish_color_escape bryellow --bold
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_host_remote yellow
+set -U fish_color_match --background=brblue
+set -U fish_color_normal normal
+set -U fish_color_operator bryellow
+set -U fish_color_param cyan
+set -U fish_color_quote yellow
+set -U fish_color_redirection brblue
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
+
 # - Packages -
 
 # Install and run Fisher package manager on first run
