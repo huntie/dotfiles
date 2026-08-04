65951146d884a2d565b9e20d3a591cc15de94947
Alex Hunt <hello@alexhunt.dev>
2026-08-04 12:06:16 +0100

    macos: Update key for StartupMute

--- macos/.macos-prefs/sound.sh
+++ macos/.macos-prefs/sound.sh
@@ -1,5 +1,5 @@
 # Disable sound effects on boot
-sudo nvram SystemAudioV
+sudo nvram StartupMute=%01
 
 # Disable user interface sound effects
 defaults write com.apple.systemsound com.apple.sound.uiaudio.enabled -bool false
