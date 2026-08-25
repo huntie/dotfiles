ca394145074de85786195c708df284857621acd3
Alex Hunt <hello@alexhunt.dev>
2026-08-25 12:10:34 +0100

    vscode: Sync extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,17 +1,42 @@
 code --install-extension adamsome.vscode-theme-gruvbox-minor
 code --install-extension anthropic.claude-code
 code --install-extension brian-anders.sublime-duplicate-text
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension EditorConfig.EditorConfig
-code --install-extension esbenp.prettier-vscode
-code --install-extension GitHub.copilot
-code --install-extension GitHub.copilot-chat
-code --install-extension GitHub.vscode-pull-request-github
-code --install-extension mikestead.dotenv
+code --install-extension britesnow.vscode-toggle-quotes
+code --install-extension fabiospampinato.vscode-open-in-github
+code --install-extension meta.sapling-scm
 code --install-extension mk12.better-git-line-blame
 code --install-extension ryuta46.multi-command
 code --install-extension sleistner.vscode-fileutils
 code --install-extension usernamehw.errorlens
 code --install-extension vscodevim.vim
 code --install-extension wmaurer.change-case
+
+# Core languages/formatters
+code --install-extension danvk.any-xray
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension flowtype.flow-for-vscode
+code --install-extension harrydowning.yaml-embedded-languages
+code --install-extension mathiasfrohlich.kotlin
+code --install-extension mikestead.dotenv
+code --install-extension ms-vscode.cpptools
+code --install-extension redhat.vscode-yaml
 code --install-extension xaver.clang-format
+code --install-extension yoavbls.pretty-ts-errors
+code --install-extension zchrissirhcz.cmake-highlight
+
+# Extra languages (dotfiles)
+# code --install-extension bmalehorn.vscode-fish
+# code --install-extension malmaud.tmux
+# code --install-extension xadillax.viml
+
+# Extra languages (web)
+# code --install-extension bradlc.vscode-tailwindcss
+# code --install-extension unifiedjs.vscode-mdx
+# code --install-extension bmewburn.vscode-intelephense-client
+# code --install-extension cjhowe7.laravel-blade
+
+# Extra languages (chrome-devtools-frontend)
+# code --install-extension google.gn
+# code --install-extension runem.lit-plugin
